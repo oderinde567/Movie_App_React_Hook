1067ace6f22f568b4ba6e3b19bae1155df2dab69
oderinde567 <oderinde567@gmail.com>
2025-12-25 19:32:10 +0100

    updated

--- App.js/javascript.js
+++ App.js/javascript.js
@@ -3,20 +3,22 @@ import MovieList from "./MovieList";
 import Filter from "./Filter";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      title: "Inception",
-      description: "A mind-bending thriller by Christopher Nolan.",
-      posterURL: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
-      rating: 5,
-    },
-    {
-      title: "The Matrix",
-      description: "A computer hacker learns about the true nature of reality.",
-      posterURL: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
-      rating: 4,
-    },
-  ]);
+const [movies, setMovies] = useState([
+  {
+    title: "Inception",
+    description: "A mind-bending thriller by Christopher Nolan.",
+    posterURL: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
+    rating: 5,
+    trailer: "https://www.youtube.com/embed/YoHD9XEInc0"
+  },
+  {
+    title: "The Matrix",
+    description: "A computer hacker learns about the true nature of reality.",
+    posterURL: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
+    rating: 4,
+    trailer: "https://www.youtube.com/embed/vKQi3bBA1y8"
+  },
+]);
 
   const [filterTitle, setFilterTitle] = useState("");
   const [filterRating, setFilterRating] = useState(0);
